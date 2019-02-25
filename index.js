3a9beefda0e4fbe4604300e742c548161ad5feb6
onrush1995 <moniruzzaman.eng1995@gmail.com>
2019-02-25 20:41:11 +0200

    Students has been added

--- index.js
+++ index.js
@@ -6,35 +6,80 @@ const app = express();
 
 const schema = gql`
   type Query {
-    user: User,
-    users: [User!]!
+    Student: Student,
+    students: [Student!]!
   }
 
-  type User {
+
+  type Student {
     id: ID!,
-    email: String!
+    name: String!,
+    email: String!,
+    Street: String!,
+    PostCode: String!,
+    Country: String!,
+    firstName: String!,
+    familyName: String!,
+    birthday: String,
   }
-`;
+  `;
 
-let users = [
+  let students = [
   {
     id: "1",
-    email: "test.user@email.com",
+    email: "jhon.sina@students.oamk.fi",
+    firstName: "Jhon",
+    familyName: "Sina",
+    Street: "Kotkantie 1",
+    PostCode: "90250",
+    Country: "Finland",
+    birthday: "10-12-1997",
   },
   {
     id: "2",
-    email: "john.doe@email.com"
-  }
-];
+    email: "Fore.Ian@students.oamk.fi",
+    firstName: "Fore",
+    familyName: "Ian",
+    Street: "Oxford Street 79",
+    PostCode: "90118",
+    Country: "England",
+    birthday: "07-09-1996",
+  },
+  {
+  id: "3",
+  email: "Barbara.Okely@students.oamk.fi",
+  firstName: "Barbara",
+  familyName: "Okely",
+  Street: "Washington street 092",
+  PostCode: "30701",
+  Country: "USA",
+  birthday: "01-03-1995",
+},
+{
+  id: "4",
+  email: "Scott.Peak@students.oamk.fi",
+  firstName: "Scott",
+  familyName: "Peak",
+  Street: "Kajaaninte 34",
+  PostCode: "90130",
+  Country: "Finland",
+  birthday: "06-07-1994",
+},
 
+];
 
 const resolvers = {
   Query: {
-    user: (parent, args, context, info) => {
-      return users[0];
+    Student: (parent, args, context, info) => {
+      return students[0];
+    },
+    students: (parents, args, context, info) => {
+      return students;
     }
   }
 };
+
+
 
 const server = new ApolloServer({
   typeDefs: schema,
@@ -48,6 +93,8 @@ const server = new ApolloServer({
     return response;
   },
 });
+
+
 
 server.applyMiddleware({ app, path: '/graphql' });
 
